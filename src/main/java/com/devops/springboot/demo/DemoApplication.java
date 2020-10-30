ea9ee41ef63514fdacba4fc0032e167b7b73e015
Gajalakshmi <gn35@dxc.com>
2020-10-30 18:02:53 +0530

    Starters class added

--- src/main/java/com/devops/springboot/demo/DemoApplication.java
+++ src/main/java/com/devops/springboot/demo/DemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		
+		Starters.startH2Server();
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
---
+++ src/main/java/com/devops/springboot/demo/Starters.java
@@ -0,0 +1,25 @@
+package com.devops.springboot.demo;
+
+import java.sql.SQLException;
+
+import org.h2.tools.Server;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Starters {
+ 
+    private static final Logger logger = LoggerFactory.getLogger(Starters.class);
+ 
+    public static void startH2Server() {
+        try {
+            Server h2Server = Server.createTcpServer().start(); // key code
+            if (h2Server.isRunning(true)) {
+                logger.info("H2 server was started and is running.");
+            } else {
+                throw new RuntimeException("Could not start H2 server.");
+            }
+        } catch (SQLException  e) {
+            throw new RuntimeException("Failed to start H2 server: ", e);
+        }
+    }
+}
