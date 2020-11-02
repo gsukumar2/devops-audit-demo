1f378bf71a6363f113b2246f9d631e6cd130d16c
Gajalakshmi <gn35@dxc.com>
2020-11-03 01:15:27 +0530

    revereted to h2 in mem

--- src/main/java/com/devops/springboot/demo/DemoApplication.java
+++ src/main/java/com/devops/springboot/demo/DemoApplication.java
@@ -1,23 +1,17 @@
 package com.devops.springboot.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication extends SpringBootServletInitializer{
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		// To start H2 TCP server on Spring Boot application startup?
-		//Starters.startH2Server();
 		SpringApplication.run(DemoApplication.class, args);
 	}
-   /* @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-    	Starters.startH2Server();
-        return application.sources(DemoApplication.class);
-    }
-
-*/
+ 
 }
