8beb5a872b1f86878398ca3ac72c816bb0769bf0
Gajalakshmi <gn35@dxc.com>
2020-11-02 08:17:26 +0530

    Main class modified

--- src/main/java/com/devops/springboot/demo/DemoApplication.java
+++ src/main/java/com/devops/springboot/demo/DemoApplication.java
@@ -2,14 +2,22 @@ package com.devops.springboot.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		// To start H2 TCP server on Spring Boot application startup?
 		Starters.startH2Server();
 		SpringApplication.run(DemoApplication.class, args);
 	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+    	Starters.startH2Server();
+        return application.sources(DemoApplication.class);
+    }
+
 
 }
