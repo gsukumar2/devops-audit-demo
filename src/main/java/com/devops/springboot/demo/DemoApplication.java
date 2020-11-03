8da44a2ea7d1941ed470e9a14e8d8dcea7267275
Gajalakshmi <gn35@dxc.com>
2020-11-03 09:50:53 +0530

    logger added

--- src/main/java/com/devops/springboot/demo/DemoApplication.java
+++ src/main/java/com/devops/springboot/demo/DemoApplication.java
@@ -1,17 +1,20 @@
 package com.devops.springboot.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class DemoApplication {
+	
+	private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
 
 	public static void main(String[] args) {
+		
+		logger.info("DevOps application started");
 		SpringApplication.run(DemoApplication.class, args);
+		logger.info("DevOps application started");
 	}
  
 }
