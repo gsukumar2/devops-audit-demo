c67da380f9ab726cd3257e7b0a36c754abe9f46c
Gajalakshmi <gn35@dxc.com>
2020-11-03 10:42:33 +0530

    sysout added

--- src/main/java/com/devops/springboot/demo/DemoApplication.java
+++ src/main/java/com/devops/springboot/demo/DemoApplication.java
@@ -1,16 +1,13 @@
 package com.devops.springboot.demo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-	
 
 	public static void main(String[] args) {
-		
+		System.out.println(" Application starting");
 		SpringApplication.run(DemoApplication.class, args);
 	}
  
